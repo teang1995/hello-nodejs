e0442c9f93399ba3b164c10c0b5ac37370f15a8d
teang1995 <teang1995@ajou.ac.kr>
2022-04-13 07:04:02 +0000

    [#1] add simple REST API at demo/routes/index.js

--- demo/routes/index.js
+++ demo/routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET api demo*/
+router.get('/api/demo', function(req, res) {
+  res.status(200).json({
+    'message': 'call get api demo'
+  });
+});
+
+/* POST api demo */
+router.post('/api/demo', function(req, res) {
+  res.status(200).json({
+    'message': 'call post api demo'
+  });
+});
 module.exports = router;
